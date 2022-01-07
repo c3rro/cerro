afcf40a6696463be79b75da8823ee74e848d18b7
Jen Rogers <jen@sci.utah.edu>
2022-01-06 21:23:58 -0700

    added waiting page

--- src/components/routes/home/HomePage.tsx
+++ src/components/routes/home/HomePage.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Button } from '@mantine/core';
 
 export default function HomePage() {
   return (
-    <div>
-      <h1 style={{ fontSize: 50, fontWeight: 'bold', textAlign: 'center' }}>
-        React Pages Boilerplate
+    <div style={{marginLeft:100}}>
+      <h1 style={{ fontSize: 80, fontWeight: 'bold', textAlign: 'left', color: 'gray', marginTop:200 }}>
+        Hello there!
       </h1>
-
-      <Button>A button</Button>
+      <h2 style={{ fontSize: 60, fontWeight: 'bold', textAlign: 'left', color: '#BEBEBE' }}>Thanks for stopping by. Until our website is finished, </h2>
+      <h2 style={{ fontSize: 60, fontWeight: 'bold', textAlign: 'left', color: '#BEBEBE', lineHeight: .1 }}>you can say hi at <span style={{color:'gray'}}>cerroanalytics (at) gmail (dot) com.</span></h2>
     </div>
   );
 }
